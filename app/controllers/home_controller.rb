b05dcb85aabb2fca0e306a2664f751b2355f2ae1
Alauddin Ulaganathan <alauddin.ulaganathan@gmail.com>
2021-12-29 23:04:50 +0530

    added back db changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,15 @@ class HomeController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:contact]
 
   def index
+    puts('index')
   end
 
   def contact
+    contact = Contact.new(
+      name: params['name'], email: params['email'],
+      phone: params['phone'], message: params['message']
+    )
+    contact.save
     ContactMailer.with(contact: contact).contact_email.deliver_now
     redirect_to root_path
   end
